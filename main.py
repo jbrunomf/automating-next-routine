1a6f5b93b20d4b4c4ff0587f5dffb34684b2c11c
João Bruno M. Fernandes <jbrunomf@outlook.com>
2021-04-21 12:03:53 -0400

    Refatorando

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ import time
 import subprocess
 import psutil
 
+
 EXE_PATH = 'C:\\Next\\AgendamentoNext\\Next_Agendamento.exe'
 PROCESS_NAME = 'Next_Agendamento.exe'
 
 
-def verify_if_popen(pname):
+def check_if_popen(pname):
     """
     Verifica se existe algum processo ativo cujo o attr name seja == ao parâmetro pname.
     Caso exista, o processo será finalizado.
@@ -34,37 +35,49 @@ def start_process():
     Exibe mensagem de alerta, indicando o início do processo.
     :return:
     """
-    # pyautogui.alert('Iniciando rotina - Next Agendamento. Pressione OK para continuar o processo.\nNão faça uso da máquina até que a mensagem de sucesso'
-    #                 'Seja exibida!')
-    # time.sleep(1)
-    subprocess.Popen(EXE_PATH)
-    time.sleep(10)
-    check_options()
+    try:
+        subprocess.Popen(EXE_PATH)
+        time.sleep(10)
+        check_options()
+    except OSError as err:
+        print(err)
 
 
 def check_options():
     """
-    Procura pelo conteúdo da imagem 'ligado.png', e clica na área indicada.
+    Procura pelo conteúdo da imagem 'check.png', e clica na área indicada (imagem).
     Caso a imagem não seja encontrada, retorna ImageNotFoundException.
     :return:
     """
     try:
-        turn_on_label_img = pyautogui.locateOnScreen('img/ligado.png')
+        turn_on_label_img = pyautogui.locateOnScreen('check.png')
         pyautogui.click(turn_on_label_img)
         time.sleep(2)
-        turn_on_label_img = pyautogui.locateOnScreen('img/ligado.png')
+        turn_on_label_img = pyautogui.locateOnScreen('check.png')
         # Marcar opção buscar agendamento
         pyautogui.click(turn_on_label_img)
         time.sleep(2)
-        # Minimizar App
-        pyautogui.click(1103, 427)
-        time.sleep(2)
-    except Exception as e:
+    except pyautogui.ImageNotFoundException as e:
         print(str(e))
+    minimize()
+
+
+def minimize():
+    """
+    Minimiza a janela do app Agendamento.exe. (windows api call)
+    :return:
+    """
+    try:
+        window_list = pyautogui.getWindowsWithTitle('Next - Agendamento')
+        for window in window_list:
+            if window.title == 'Next - Agendamento':
+                window.minimize()
+    except Exception as e:
+        print(e)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    verify_if_popen(PROCESS_NAME)
+    check_if_popen(PROCESS_NAME)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
